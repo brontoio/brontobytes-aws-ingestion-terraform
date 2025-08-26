a25c459a7ee3b61d773dc4b59d0583c81275dba5
Benoit Gaudin <benoit@logchat.io>
2025-08-27 00:18:08 +0100

    BRONTO-1948 decouple logging bucket and artefact bucket permissions

--- aws_log_forwarder/iam.tf
+++ aws_log_forwarder/iam.tf
@@ -12,6 +12,30 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "s3_artefact_access" {
+
+  statement {
+    effect = "Allow"
+    resources = [
+      "${local.artefact_bucket["arn"]}/${var.name}/*",
+      "${local.artefact_bucket["arn"]}/${local.otel_config_s3_key}",
+      "${local.artefact_bucket["arn"]}/${local.destination_config_s3_key}",
+      "${local.artefact_bucket["arn"]}/${local.paths_regex_config_s3_key}"
+    ]
+    actions   = ["s3:Get*", "s3:List*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = [local.artefact_bucket.arn]
+    actions   = ["s3:ListBucket"]
+  }
+}
+
+resource "aws_iam_policy" "s3_artefact_access" {
+  policy = data.aws_iam_policy_document.s3_artefact_access.json
+}
+
 resource "aws_iam_role" "this" {
   count              = local.create_role ? 1 : 0
   name               = local.role_name
@@ -20,6 +44,12 @@ resource "aws_iam_role" "this" {
 
 resource "aws_iam_role_policy_attachment" "basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = local.role_name
+  depends_on = [aws_iam_role.this]
+}
+
+resource "aws_iam_role_policy_attachment" "s3_artefact_access" {
+  policy_arn = aws_iam_policy.s3_artefact_access.arn
   role       = local.role_name
   depends_on = [aws_iam_role.this]
 }
